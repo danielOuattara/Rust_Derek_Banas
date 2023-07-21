1881d0f722115d9641fbbefdd6195e8c5454c946
danielOuattara <daniel.ouattara@gmx.com>
2023-07-21 23:27:04 +0200

    12 casting

---
+++ src/_12_casting.rs
@@ -0,0 +1,13 @@
+// time @50'28"
+
+pub fn casting() {
+    let int_1_u8: u8 = 5;
+    let int_2_u8: u8 = 4;
+
+    // casting using 'as'
+    let int_3_u32: u32 = (int_1_u8 as u32) + (int_2_u8 as u32);
+
+    // let string_numbered = "1";
+    // let casted_string = (string_numbered as u8);
+    // println!("{casted_string}")
+}
